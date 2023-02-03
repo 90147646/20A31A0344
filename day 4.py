5aa3e60f6c2b34c671e06188d8b95e09eba99bfc
90147646 <113779795+90147646@users.noreply.github.com>
2023-02-03 10:06:20 +0530

    Add files via upload

--- day 4.py
+++ day 4.py
@@ -53,6 +53,95 @@ print(str.lower())
 str1='Indian'
 print(str1[::])
 
+import string
+print(string.digits)
+print(string.ascii_letters)
+chr='g'
+print('a'<=chr<='z')
+
+
+str1='hello'
+print(dir(str1))
+
+
+import re
+str1="she sells sea shells at sea shore"
+p1="sells"
+if re.match(p1,str1):
+        print("match found")
+else:
+        print(p1,"not present in the string")
+
+
+import re
+str1="she sells sea shells at sea shore"
+p1="sells"
+if re.search(p1,str1):
+        print("match found")
+else:
+        print(p1,"not present in the string")
+
+import re
+str1="she sells sea shells at sea shore"
+p1="sea"
+rep="ocean"
+ns=re.sub(p1,rep,str1,1)
+print(ns)
+
+import re
+p=r"[aeiou]"
+if re.search(p,"clue"):
+        print("mathcy vowel")
+
+
+import re
+p=r"[consonants]"
+if re.search(p,"clay"):
+        print("matchy consonants")
+else:
+        prinrt("not matchy consonants")
+
+n=int(input("enter the number:"))
+a=0
+b=0
+for i in range(1,n+1):
+        if(i%2!=0):
+                a=a+2
+else:
+        b=b+1
+if(n%2!=0):
+        print('{}'.format(a-2))
+else:
+        print('{}'.format(b-1))
+
+
+
+size=int(int(input("enter the size of bin")))
+max=count=flag=0
+x=input()
+arr=list(x)
+for i in range (0,size):
+        if arr[1]=='1':
+                count=count+1
+                flag=1
+        elif(arr[1]=='0' and flag==1):
+                count=0
+                flag=0
+        if count>max:
+                max=count
+print(max)
+
+                
+                
+        
+
+
+
+
+
+
+
+
 
 
 
